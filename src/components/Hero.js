1649451e2816856d90dff4068c42dd95b5a114d4
Mike Mahon <mikemahon@Mikes-MacBook-Pro.local>
2018-08-31 15:31:39 -0500

    changed pics

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import meadow from '../img/meadow.jpg';
+import clouds from '../img/clouds.jpg';
 
 const Hero = () => {
   return (
     <section
       id="hero"
       className="blue lighten-4"
-      style={{ backgroundImage: 'url(' + meadow + ')'}}
+      style={{ backgroundImage: 'url(' + clouds + ')'}}
     >
       <div className="container valign-wrapper jc-center">
         <div className="valign center-align white-text">
-          <p className="flowtext hide-on-small-only">We give your business an internet presence</p>
+          <p className="flowtext hide-on-small-only">Migrate your company's assets to the cloud</p>
           <h3>
-            Develop, Deploy, Done
+            Cloud Migration Experts
           </h3>
 
           <p className="big">
-            We build your website using cutting edge frameworks
+            Avoid the complications of cloud migration
             <br/>
-            entirely customized and made to order
+            Migrate your company's assets to the cloud quickly and safely
           </p>
 
           <a className="btn waves-light waves-effect m-r-16">Features</a>
